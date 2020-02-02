8cccbdd92389a12a29d5b9df055998e32f3dfd72
IHSSAT <patrick.yuan@icsd.k12.ny.us>
2020-02-02 00:16:17 -0500

    update dumping method to pickle

--- data collection/fetch_data.py
+++ data collection/fetch_data.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
-import json
 import re 
 from urllib.request import urlopen, Request, urlretrieve
+import pickle 
 
 # player = open("towns.html", encoding="utf8")
 
@@ -25,14 +25,15 @@ def get_player_urls(team):
     return player_urls
 
 def player_url_to_name(url):
-    return url.split("/")[0]
+    return url.split("/")[-1]
 
 def get_player_number(player):
     # Takes an opened html file as input, which should be from a player's page. Returns a dict that maps week number to player rating.
     soup = BeautifulSoup(player, 'html.parser')
     player_num = soup.findAll("h2", {"class": "player-jersey"})[0]
-    player_num = player_num.string
+    player_num = str(player_num)
     player_num = player_num[player_num.index("#") + 1:player_num.index("|")]
+    # print(player_num)
     return int(player_num)
 
 def download_webpage(url):
@@ -63,25 +64,65 @@ def get_player_ratings(player):
 
 def dump_to_json(teams, year):
     # Takes an opened html file as input, which should be from the page that lists all the teams.
-    # returns a json file in the following format:
+    # returns a dict in the following format:
     #
     # year
 	#   team
 	# 	    player number
 	# 		    player name
 	# 		    ratings
+    # It also returns a dict in the following format
+    # year
+    #   player_name
+    #       player_num
+    #       ratings
 
     data = dict()
     data[year] = dict()
+    data_player_names_only = dict()
+    data_player_names_only[year] = dict()
+
     team_urls = get_team_urls(teams)
-    for team in team_urls:
-        team_urls = get_team_urls(team)
-        for team_url in team_urls:
-            pass
+    for team_url in team_urls:
+        if team_url == "https://nba2k19.2kratings.com/team/boston-celtics":
+            break
+            # pass
+        try:
+            team = download_webpage(team_url)
+            team_name = team_url_to_name(team_url)
+            data[year][team_name] = dict()
+            player_urls = get_player_urls(team)
+            print("TEAM: ", team_name)
+            for player_url in player_urls:
+                player = download_webpage(player_url)
+                player_name = player_url_to_name(player_url)
+                print("PLAYER: ", player_name)
+                player_num = get_player_number(player)
+                player_ratings = get_player_ratings(player)
+                data[year][team_name][player_num] = dict()
+                data[year][team_name][player_num]["player_name"] = player_name
+                data[year][team_name][player_num]["player_ratings"] = player_ratings
+                data_player_names_only[year][player_name] = dict()
+                data_player_names_only[year][player_name]["player_number"] = player_num
+                data_player_names_only[year][player_name]["player_ratings"] = player_ratings
+        except Exception as e:
+            print(e)
+    return data, data_player_names_only
 
 
 # print(get_player_urls(team))
+# player = download_webpage("https://nba2k19.2kratings.com/player/kyrie-irving")
+# print(get_player_number(player))
+if __name__ == "__main__":
+    teams = download_webpage("https://nba2k19.2kratings.com/current-teams-on-nba-2k19")
+    data, data_player_names_only = dump_to_json(teams, 2019)
 
-player = download_webpage("https://www.2kratings.com/nba2k20/karl-anthony-towns")
-print(get_player_number(player))
-print(get_player_ratings(player))
+    output = open("2k19.p", "wb")
+    pickle.dump(data, output, protocol=pickle.HIGHEST_PROTOCOL)
+    output.close()
+
+    output = open("2k19_player_names_only.p", "wb")
+    pickle.dump(data_player_names_only, output)
+    output.close()
+
+
